5ca8890c4899b9b6eee82afbf6e6be303981e236
aarugunj27 <aarugunj27@ncs.charter.k12.de.us>
2022-09-21 19:51:02 -0400

    NavBar

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './static/css/style.css';
+
+function Home() {
+    return (
+        <div>
+            <title>Home</title>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home;
